9d76fae49338b232a9de3b6d7c2372576d9d9861
IGOR_DUARTE_DE_MATOS_MADUREIRA <71901027@ALFAB48.BARROCA>
2022-05-16 20:26:55 -0300

    start template css views

--- src/views/TestsView.vue
+++ src/views/TestsView.vue
@@ -1,15 +1,28 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="viewLayout">
+    <h1>Verificação, validação e testes</h1>
+    <h2>Visões da qualidade</h2>
+    <p>Para termos qualidade devemos seguir algumas visões:</p>
+    <ul>
+      <li class="list">
+        <p>Usuário: Deve ter facilidade de uso, desempenho, confiabilidade dos resultados, preço do software, etc</p>
+      </li>
+      <li>
+        <p>Desenvolvedor: Taxa de defeitos, facilidade de manutenção e conformidade em relação aos requisitos de usuários, etc</p>
+      </li>
+      <li>
+        <p>Organização: Cumprimento de prazo, boa previsão de custo, boa produtividade.</p>
+      </li>
+    </ul>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+@import '@/assets/viewsEssentials.css';
 </style>
