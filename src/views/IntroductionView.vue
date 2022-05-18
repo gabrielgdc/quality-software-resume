f4ab41d6a5ce69ec0cd56bf4355fff0ea068e361
igoohd <igor.duartematos@gmail.com>
2022-05-18 10:57:20 -0300

    feat: Finish introduction view

--- src/views/IntroductionView.vue
+++ src/views/IntroductionView.vue
@@ -1,28 +1,81 @@
 <template>
-<div class="viewLayout">
+  <div class="viewLayout">
     <h1>Introdução Qualidade de Software</h1>
-    <h2>titulo 2</h2>
-    <p>paragráfo descritivo</p>
+    <h2>Porque qualidade de software?</h2>
+    <p>
+      Qualidade de software é importante pois nos ajuda à evitar grandes
+      problemas em meios corporativos que tem relação direta com
+      tecnologia(atualmente praticamente todos), alguns desses problemas são:
+    </p>
     <ul>
       <li class="list">
-        <p>item 1</p>
+        <p>Perda de negócios</p>
       </li>
       <li>
-        <p>item 2</p>
+        <p>Perda de dados</p>
       </li>
       <li>
-        <p>item 3</p>
+        <p>Perda de tempo</p>
+      </li>
+      <li>
+        <p>Perda de dinheiro</p>
       </li>
     </ul>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
+    <p>
+      A falta de qualidade geram falhas que podem impactar negativamente todo um
+      negócio dependendo da frequência de uso, das responsabilidades e da
+      importância do mesmo.
+    </p>
+    <p>
+      Falhas de software acontecem no mundo todo e tivemos diversos desastres
+      por conta delas, como por exemplo o Foguete Mariner. Um foguete que
+      explodiu por falta de um hífen, dando um prejuízo de US$80 milhões para a
+      empresa. O hífen foi omitido num código matemático escrito à mão e deu um
+      fim trágico à um projeto de tamanha proporção.
+    </p>
+    <p>
+      A definição de qualidade e software varia dependendo do tipo do sistema,
+      do avaliador e de outros fatores, mas pode ser medida de uma mesma
+      maneira: a forma com que o sistema, componente ou processo atende os
+      requisitos especificados e as expectativas e necessidades do cliente ou
+      usuário.
+    </p>
+    <p>Fatores que afetam a qualidade do software são:</p>
+    <ul>
+      <li class="list">
+        <p>Tecnologia de desenvolvimento</p>
+      </li>
+      <li>
+        <p>Qualidade das pessoas</p>
+      </li>
+      <li>
+        <p>Gerência</p>
+      </li>
+      <li>
+        <p>Qualidade do processo</p>
+      </li>
+    </ul>
+    <p>
+      Para conquistarmos esses fatores temos modelos de qualidade responsáveis
+      por definir as características de qualidade que um software ou sistema
+      computacional deva conter, auxiliando na especificação dos requisitos e na
+      medição e avaliação da qualidade.
+    </p>
+    <p>
+      Cada modelo de qualidade possui uma característica que é a base para a
+      avaliação do produto de software sob determinado ponto de vista, modelos
+      diferentes trazem diferentes métricas de avaliação: Desempenho, segurança,
+      usabilidade e etc.
+    </p>
+    <p>
+      A motivação para a criação de modelos de qualidade de software são os
+      benefícios como: aumento da qualidade do produto, diminuição do
+      retrabalho, maior produtividade, redução de tempo para atender o mercado,
+      competitividade e precisão nas estimativas.
+    </p>
   </div>
 </template>
 
 <style>
-@import '@/assets/viewsEssentials.css';
+@import "@/assets/viewsEssentials.css";
 </style>
