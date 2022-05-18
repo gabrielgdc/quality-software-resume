20111a57676b5a4b293f5e0d51bd85f056e4ede4
igoohd <igor.duartematos@gmail.com>
2022-05-18 12:26:02 -0300

    feat: Finish CMMIView

--- src/views/CmmiView.vue
+++ src/views/CmmiView.vue
@@ -1,28 +1,69 @@
 <template>
-<div class="viewLayout">
+  <div class="viewLayout">
     <h1>CMMI</h1>
-    <h2>titulo 2</h2>
-    <p>paragráfo descritivo</p>
+    <h2>Melhoria de processo de software</h2>
+    <p>
+      A qualidade de um software está diretamente ligada a qualidade do processo
+      de desenvolvimento, um processo está falho quando tem os seguintes
+      sintomas: compromissos não cumpridos, falta de visibilidade no progresso
+      durante o desenvolvimento, problemas de qualidade e pouca motivação
+    </p>
+    <p>
+      Para ajudar nesse problema temos modelos de melhoria de processo de
+      software, responsávei por estruturar genericamente uma melhoria de
+      processo descrevendo fases, atividades e recursos necessários para um
+      esforço bem sucedido, avaliando a qualidade de empresas que implementam
+      processos aderentes às práticas do CMMI.
+    </p>
+    <p>O modelo CMMI é um desses modelos e possui 5 níveis:</p>
     <ul>
       <li class="list">
-        <p>item 1</p>
+        <p>Inicial</p>
       </li>
       <li>
-        <p>item 2</p>
+        <p>Gerenciado</p>
       </li>
       <li>
-        <p>item 3</p>
+        <p>Definido</p>
+      </li>
+      <li>
+        <p>Gerenciado Quantativamente</p>
+      </li>
+      <li>
+        <p>Otimizado</p>
       </li>
     </ul>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia cumque adipisci deleniti rerum fuga illum rem consectetur mollitia quibusdam sit! Iusto sint illum exercitationem error optio accusamus pariatur omnis at.</p>
+    <p>
+      Cada um dos níveis são organizados para estabelecer as prioridades na
+      condução de programas de melhoria de sw, e cada nível é pré-requisito para
+      o nível seguinte, sendo aplicado de forma linear.
+    </p>
+    <p>
+      No nível inicial os processos não são bem definidos e não tem planos
+      adequados, portanto acontecem de forma improvisada e aparecem muitos
+      erros.
+    </p>
+    <p>
+      No nível gerenciado o status dos produtos e serviços é visível para a
+      gerência em determinados pontos do processo.
+    </p>
+    <p>
+      No nível definido funções e responsabilidades são bem entendidas e a
+      produção do produto é visível por meio do processo de software.
+    </p>
+    <p>
+      No nível gerenciado quantativamente a gerência tem bases objetivas para
+      tomada de decisão e é capaz de prever o desempenho dentro de limites
+      quantificados.
+    </p>
+    <p>
+      No nível Otimizado as organizações focam em melhoria contínua para
+      alcançar processos flexíveis capazes de atender às oportunidades e
+      mudanças do meio corporativo.
+    </p>
   </div>
 </template>
 
 <style>
-@import '@/assets/viewsEssentials.css';
+@import "@/assets/viewsEssentials.css";
 </style>
